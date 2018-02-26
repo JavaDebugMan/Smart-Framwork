c524e53653fa1cbdaa5920236e8d1addee01b4c7
pengzhe <pengzhepost@sina.com>
2018-02-26 22:37:07 +0800

    完成view

--- src/main/java/com/javaman/controller/CustomerServlet.java
+++ src/main/java/com/javaman/controller/CustomerServlet.java
@@ -1,10 +1,35 @@
 package com.javaman.controller;
 
+import com.javaman.model.Customer;
+import com.javaman.service.CustomerService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
 /**
  * @author pengzhe
  * @date 2018/2/26 17:19
  * @description
  */
+@WebServlet("/customer")
+public class CustomerServlet extends HttpServlet {
 
-public class CustomerServlet {
+    private CustomerService customerService;
+
+    @Override
+    public void init() throws ServletException {
+        customerService = new CustomerService();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<Customer> customerList = customerService.getCustomerList();
+        req.setAttribute("customerList", customerList);
+        req.getRequestDispatcher("/WEB-INF/view/customer.jsp").forward(req, resp);
+    }
 }
